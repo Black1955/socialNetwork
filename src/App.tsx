18918818d2051144bc12952cddfc388989fa1cd6
Denis <babadid346@gmail.com>
2023-04-28 23:46:47 +0300

    post component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import "./App.scss";
-import { FC } from "react";
+import { FC, useState } from "react";
+import Post from "./components/Post/Post.tsx";
+
 
 const App: FC = () => {
-  return <div></div>;
+
+  const [liked,setLiked]=useState(false)
+  const [comented,setComented]=useState(false)
+  const handlerLiked=()=>{
+    setLiked(!liked)
+  }
+  const handlerComented=()=>{
+    setComented(!comented)
+  }
+
+  
+
+  return <div>
+    <Post description="Its a good pet for me" id="1"  folowed={false} imgUrl="https://www.imgonline.com.ua/examples/bee-on-daisy.jpg" comented={comented}setComented={handlerComented} setLiked={handlerLiked} liked={liked} likes={123} name="vasya" time="6 days ago"/>
+  </div>;
 };
 
 export default App;
