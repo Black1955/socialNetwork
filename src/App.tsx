12c60638ded00a90da4d8d7c03ce71463dd11883
Denmaks234 <denmaks432@gmail.com>
2023-04-29 00:27:39 +0300

    commit be

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,4 @@ import { FC } from "react";
 const App: FC = () => {
   return <div></div>;
 };
-
-<<<<<<< HEAD
 export default App;
-=======
-
-export default App;
-
->>>>>>> e4460459a15c319d583ebe82adc9ddc9b6db987b
