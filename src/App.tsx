7d32d4ed695f3a81a97bda3d3567896bdee801ca
Denis <babadid346@gmail.com>
2023-04-29 00:28:44 +0300

    slavik

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import "./App.scss";
 import { FC } from "react";
-
+import RecommendedBlog from "./components/RecommendedBlog/RecommendedBlog";
 const App: FC = () => {
-  return <div></div>;
+  return <div>
+    <RecommendedBlog desc="ejcnwel" userName="vasya"/>
+  </div>;
 };
 
-<<<<<<< HEAD
-export default App;
-=======
 
 export default App;
 
->>>>>>> e4460459a15c319d583ebe82adc9ddc9b6db987b
