5d128c650d110fb67e7bc5b8a390fc1da8728159
Denmaks234 <denmaks432@gmail.com>
2023-04-30 01:23:03 +0300

    clear add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import "./App.scss";
 import { FC } from "react";
-import LinksBar from "./components/LinksBar/LinksBar";
-import Header from "./components/Header/Header";
 const App: FC = () => {
   return <div>
- <Header/>
   </div>;
 }
 export default App;
