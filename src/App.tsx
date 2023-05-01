09e000cceb64ff271c474806a884b8474b4c3b5e
Denmaks234 <denmaks432@gmail.com>
2023-05-01 23:41:28 +0300

    clear App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import "./App.scss";
-import { FC, useState } from "react";
-import SignUpForm from "./components/SignUp/SignUpForm";
-
+import { FC } from "react";
 import "./App.scss";
 const App: FC = () => {
- 
-
-  return <div>
-    <SignUpForm   />
-
-  </div>;
+  return <div></div>;
 };
 
 export default App;
